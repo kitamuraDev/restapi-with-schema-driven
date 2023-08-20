e758e3deb275ae3c789885c14128e8b7808bf1c2
kitamuraDev <masato1999824@icloud.com>
2023-08-20 15:52:00 +0900

    :truck: メソッドの命名を変更

---
+++ src/main/java/com/example/restapiwithschemadriven/controller/task/TaskController.java
@@ -0,0 +1,16 @@
+package com.example.restapiwithschemadriven.controller.task;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todoapi.controller.TasksApi;
+
+@RestController
+public class TaskController implements TasksApi {
+
+  @Override
+  public ResponseEntity<Void> showTask() {
+    return ResponseEntity.ok().build();
+  }
+
+}
