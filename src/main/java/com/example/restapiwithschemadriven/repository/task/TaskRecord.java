08e63effc337d4ecfb76d9e1926ee0980324b50c
kitamuraDev <masato1999824@icloud.com>
2023-08-21 23:40:21 +0900

    :sparkles: Repositoryを実装し、DBから単一のレコードを取得

---
+++ src/main/java/com/example/restapiwithschemadriven/repository/task/TaskRecord.java
@@ -0,0 +1,11 @@
+package com.example.restapiwithschemadriven.repository.task;
+
+import lombok.Value;
+
+@Value
+public class TaskRecord {
+
+  long id;
+  String title;
+
+}
--- src/main/java/com/example/restapiwithschemadriven/service/task/TaskService.java
+++ src/main/java/com/example/restapiwithschemadriven/service/task/TaskService.java
@@ -2,11 +2,20 @@ package com.example.restapiwithschemadriven.service.task;
 
 import org.springframework.stereotype.Service;
 
+import com.example.restapiwithschemadriven.repository.task.TaskRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class TaskService {
 
+  private final TaskRepository taskRepository;
+
   public TaskEntity find() {
-    return new TaskEntity(2, "from service");
+    return taskRepository.select()
+      .map(record -> new TaskEntity(record.getId(), record.getTitle()))
+      .orElseThrow(() -> new IllegalStateException("someting throw exception")); // TODO
   }
 
 }
