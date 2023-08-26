472194de3262ac163a76849adf72a0880869d83a
kitamuraDev <masato1999824@icloud.com>
2023-08-26 23:26:59 +0900

    :sparkles: 404のボディを返すようにハンドラーを修正

--- src/main/java/com/example/restapiwithschemadriven/controller/advice/CostomExceptionHandler.java
+++ src/main/java/com/example/restapiwithschemadriven/controller/advice/CostomExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.example.restapiwithschemadriven.controller.advice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.restapiwithschemadriven.service.task.TaskEntityNotFoundException;
+import com.example.todoapi.model.ResourceNotFoundError;
 
 @RestControllerAdvice
 public class CostomExceptionHandler {
@@ -16,8 +18,11 @@ public class CostomExceptionHandler {
    * @return ResponseEntity<Void>
    */
   @ExceptionHandler(TaskEntityNotFoundException.class)
-  public ResponseEntity<Void> handleTaskEntityNotFoundException(TaskEntityNotFoundException e) {
-    return ResponseEntity.notFound().build();
+  public ResponseEntity<ResourceNotFoundError> handleTaskEntityNotFoundException(TaskEntityNotFoundException e) {
+    var error = new ResourceNotFoundError();
+    error.setDetail(e.getMessage());
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
   }
 
 }
