5365ab707bfdf6880b3618ac91b56a166b0ed6e2
kitamuraDev <masato1999824@icloud.com>
2023-08-28 18:46:17 +0900

    :recycle: BadRequestErrorCreator.from をリファクタリング

--- src/main/java/com/example/restapiwithschemadriven/controller/advice/BadRequestErrorCreator.java
+++ src/main/java/com/example/restapiwithschemadriven/controller/advice/BadRequestErrorCreator.java
@@ -1,7 +1,9 @@
 package com.example.restapiwithschemadriven.controller.advice;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import com.example.todoapi.model.BadRequestError;
@@ -9,22 +11,39 @@ import com.example.todoapi.model.InvalidParam;
 
 public class BadRequestErrorCreator {
 
+  /**
+   * エラー情報を取り出して、BadRequestError#setInvalidParams でセットして BadRequestErrorオブジェクトを返す
+   *
+   * @param ex バリデーションエラーで投げられる例外
+   * @return BadRequestError
+   */
   public static BadRequestError from(MethodArgumentNotValidException ex) {
 
-    // ex.getFieldErrors からエラー情報を取り出して、List<InvalidParam>を組み立てる
-    var invalidParamList = ex.getFieldErrors()
-      .stream()
-      .map(fieldError -> {
-        var invalidParam = new InvalidParam(fieldError.getField(), fieldError.getDefaultMessage());
-        return invalidParam;
-      })
-    .collect(Collectors.toList());
+    var invalidParamList = createInvalidParamList(ex);
 
     var error = new BadRequestError();
     error.setInvalidParams(invalidParamList);
 
     return error;
 
+  }
+
+  /**
+   * ex.getFieldErrors からエラー情報を取り出して、List<InvalidParam>を組み立てる
+   *
+   * @param ex バリデーションエラーで投げられる例外
+   * @return List<InvalidParam>
+   */
+  private static List<InvalidParam> createInvalidParamList(MethodArgumentNotValidException ex) {
+    return ex.getFieldErrors()
+      .stream()
+      .map(BadRequestErrorCreator::createInvalidParam) // Xxx::xxX は method reference と言う書き方
+      .collect(Collectors.toList());
+  }
+
+  private static InvalidParam createInvalidParam(FieldError fieldError) {
+    var invalidParam = new InvalidParam(fieldError.getField(), fieldError.getDefaultMessage());
+    return invalidParam;
   }
 
 }
